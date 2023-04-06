b7ff27bb9ab963d94ead279b609672cabfbc81ad
Gokulspattom <110624065+Gokulspattom@users.noreply.github.com>
2023-04-06 09:47:19 +0530

    Changes made in dahboard

--- src/pages/DashboardAppPage.js
+++ src/pages/DashboardAppPage.js
@@ -1,219 +1,134 @@
-import { Helmet } from 'react-helmet-async';
-import { faker } from '@faker-js/faker';
 // @mui
-import { useTheme } from '@mui/material/styles';
-import { Grid, Container, Typography } from '@mui/material';
+import { Grid, Typography, Select, FormControl, InputLabel } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+
+import MenuItem from '@mui/material/MenuItem';
+import Logo from "./pictures/nova.svg";
+import Diet from "./pictures/Diet.svg";
+import Excercise from "./pictures/excercise.svg";
 // components
-import Iconify from '../components/iconify';
+
 // sections
-import {
-  AppTasks,
-  AppNewsUpdate,
-  AppOrderTimeline,
-  AppCurrentVisits,
-  AppWebsiteVisits,
-  AppTrafficBySite,
-  AppWidgetSummary,
-  AppCurrentSubject,
-  AppConversionRates,
-} from '../sections/@dashboard/app';
 
 // ----------------------------------------------------------------------
 
 export default function DashboardAppPage() {
-  const theme = useTheme();
 
   return (
-    <>
-      <Helmet>
-        <title> Dashboard | Minimal UI </title>
-      </Helmet>
+  <div >
+    <img src={Logo} alt="nova logo" style={{height: "auto", width: "250px", marginLeft: "30px"}}/>
+    <CardContent>
+    <Grid >
+      <Card sx={{ minWidth: 275 }} style={{backgroundColor:"#D1A6E7"}}>
+        <CardContent >
+          <div style={{display:'flex',flexDirection:"row",position:'relative',marginBottom:'1rem'}}>
+          <Typography color="black" variant='h5' >
+            Hello, Seema
+          </Typography>
+          <FormControl sx={{ minWidth: 120,position:'absolute',right:0 }} size="small">
+          <Select defaultValue="Today">
+            <MenuItem value="Today" >Today</MenuItem>
+            <MenuItem value="Next Week">Next Week</MenuItem>
+            <MenuItem value="Previous Week">Previous Week</MenuItem>
+          </Select>
+          </FormControl>
+          <br/>
+          </div>
 
-      <Container maxWidth="xl">
-        <Typography variant="h4" sx={{ mb: 5 }}>
-          Hi, Welcome back
-        </Typography>
 
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title="Weekly Sales" total={714000} icon={'ant-design:android-filled'} />
-          </Grid>
+          <Card sx={{ minWidth: 275 }} style={{backgroundColor:"#8D25C1"}}>
+            <Typography color="purple" variant='h4' >
+              <div style={{display:'flex',alignContent:'center',justifyContent:'center',alignItems:"center",margin: "10px"}}>
+                <span style={{color:"white" ,margin: "5px" }}>
+                  15
+                </span>
+                <span style={{color:"black" ,margin: "5px"}}>
+                    March
+                </span>
+                <span style={{color:"black",margin: "5px"}}>
+                  2023
+                </span>
+              </div>
+            </Typography>
 
-          <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title="New Users" total={1352831} color="info" icon={'ant-design:apple-filled'} />
-          </Grid>
+          </Card>
+          
+        </CardContent>
+      </Card>
+      </Grid>
+    </CardContent>
+    
+    <Grid>
+    <CardContent>
+      <Card style={{backgroundColor:"black"}} >
+        <CardContent style={{padding:"20px 15px"}}>
+          <Grid container flexDirection="row" sx={{justifyContent:'center'}} >
+          <div>
+          <Typography  color="blue" variant='h5'>
+            <div style={{marginLeft:"-15px"}}>
+              <div style={{color:"yellow" ,margin: "5px"}}>
+                120
+              </div>
+              <div style={{color:"white",margin :"5px"}}>
+                  servings
+              </div>
+              <div style={{color:"white",margin:"5px"}}>
+                recommended
+              </div>
+            </div>
+            </Typography>
+          </div>
+          <img src={Diet} alt="diet logo" style={{height: "auto", width: "auto"}}/>
+            <div>
+              <Grid container flexDirection="column"> 
+              <Typography  color="blue" variant='h5'>
+                <div style={{color:"yellow" ,margin: "5px"}}>60</div>
+                <div style={{color:"white",margin :"5px"}}>servings</div>
+                <div style={{color:"white",margin:"5px"}}>recommended</div>
+              </Typography>                   
+              </Grid>  
+            </div>    
+          </Grid>                   
+          </CardContent>
+      </Card>
+    </CardContent>
+    </Grid>
+    
+    <CardContent>
+    <Card style={{backgroundColor:"black"}} >
+        <CardContent style={{padding:"20px 15px"}}>
+          <Grid container flexDirection="row" sx={{justifyContent:'center'}}>
+          <div>
+          <Typography  color="blue" variant='h5'>
+            <div style={{marginLeft:"-15px"}}>
+              <div style={{color:"yellow" ,margin: "5px"}}>
+                20
+              </div>
+              <div style={{color:"white",margin :"5px"}}>
+                excercises
+              </div>
+              <div style={{color:"white",margin:"5px"}}>
+                recommended
+              </div>
+            </div>
+            </Typography>
+          </div>
+          <img src={Excercise} alt="excercise logo" style={{height: "auto", width: "auto"}}/>
+            <div>
+              <Grid container flexDirection="column"> 
+              <Typography  color="blue" variant='h5'>
+                <div style={{color:"yellow" ,margin: "5px"}}>10</div>
+                <div style={{color:"white",margin :"5px"}}>excercises</div>
+                <div style={{color:"white",margin:"5px"}}>left</div>
+              </Typography>                   
+              </Grid>  
+            </div>    
+          </Grid>                   
+          </CardContent>
 
-          <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title="Item Orders" total={1723315} color="warning" icon={'ant-design:windows-filled'} />
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title="Bug Reports" total={234} color="error" icon={'ant-design:bug-filled'} />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={8}>
-            <AppWebsiteVisits
-              title="Website Visits"
-              subheader="(+43%) than last year"
-              chartLabels={[
-                '01/01/2003',
-                '02/01/2003',
-                '03/01/2003',
-                '04/01/2003',
-                '05/01/2003',
-                '06/01/2003',
-                '07/01/2003',
-                '08/01/2003',
-                '09/01/2003',
-                '10/01/2003',
-                '11/01/2003',
-              ]}
-              chartData={[
-                {
-                  name: 'Team A',
-                  type: 'column',
-                  fill: 'solid',
-                  data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30],
-                },
-                {
-                  name: 'Team B',
-                  type: 'area',
-                  fill: 'gradient',
-                  data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43],
-                },
-                {
-                  name: 'Team C',
-                  type: 'line',
-                  fill: 'solid',
-                  data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39],
-                },
-              ]}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <AppCurrentVisits
-              title="Current Visits"
-              chartData={[
-                { label: 'America', value: 4344 },
-                { label: 'Asia', value: 5435 },
-                { label: 'Europe', value: 1443 },
-                { label: 'Africa', value: 4443 },
-              ]}
-              chartColors={[
-                theme.palette.primary.main,
-                theme.palette.info.main,
-                theme.palette.warning.main,
-                theme.palette.error.main,
-              ]}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={8}>
-            <AppConversionRates
-              title="Conversion Rates"
-              subheader="(+43%) than last year"
-              chartData={[
-                { label: 'Italy', value: 400 },
-                { label: 'Japan', value: 430 },
-                { label: 'China', value: 448 },
-                { label: 'Canada', value: 470 },
-                { label: 'France', value: 540 },
-                { label: 'Germany', value: 580 },
-                { label: 'South Korea', value: 690 },
-                { label: 'Netherlands', value: 1100 },
-                { label: 'United States', value: 1200 },
-                { label: 'United Kingdom', value: 1380 },
-              ]}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <AppCurrentSubject
-              title="Current Subject"
-              chartLabels={['English', 'History', 'Physics', 'Geography', 'Chinese', 'Math']}
-              chartData={[
-                { name: 'Series 1', data: [80, 50, 30, 40, 100, 20] },
-                { name: 'Series 2', data: [20, 30, 40, 80, 20, 80] },
-                { name: 'Series 3', data: [44, 76, 78, 13, 43, 10] },
-              ]}
-              chartColors={[...Array(6)].map(() => theme.palette.text.secondary)}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={8}>
-            <AppNewsUpdate
-              title="News Update"
-              list={[...Array(5)].map((_, index) => ({
-                id: faker.datatype.uuid(),
-                title: faker.name.jobTitle(),
-                description: faker.name.jobTitle(),
-                image: `/assets/images/covers/cover_${index + 1}.jpg`,
-                postedAt: faker.date.recent(),
-              }))}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <AppOrderTimeline
-              title="Order Timeline"
-              list={[...Array(5)].map((_, index) => ({
-                id: faker.datatype.uuid(),
-                title: [
-                  '1983, orders, $4220',
-                  '12 Invoices have been paid',
-                  'Order #37745 from September',
-                  'New order placed #XF-2356',
-                  'New order placed #XF-2346',
-                ][index],
-                type: `order${index + 1}`,
-                time: faker.date.past(),
-              }))}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={4}>
-            <AppTrafficBySite
-              title="Traffic by Site"
-              list={[
-                {
-                  name: 'FaceBook',
-                  value: 323234,
-                  icon: <Iconify icon={'eva:facebook-fill'} color="#1877F2" width={32} />,
-                },
-                {
-                  name: 'Google',
-                  value: 341212,
-                  icon: <Iconify icon={'eva:google-fill'} color="#DF3E30" width={32} />,
-                },
-                {
-                  name: 'Linkedin',
-                  value: 411213,
-                  icon: <Iconify icon={'eva:linkedin-fill'} color="#006097" width={32} />,
-                },
-                {
-                  name: 'Twitter',
-                  value: 443232,
-                  icon: <Iconify icon={'eva:twitter-fill'} color="#1C9CEA" width={32} />,
-                },
-              ]}
-            />
-          </Grid>
-
-          <Grid item xs={12} md={6} lg={8}>
-            <AppTasks
-              title="Tasks"
-              list={[
-                { id: '1', label: 'Create FireStone Logo' },
-                { id: '2', label: 'Add SCSS and JS files if required' },
-                { id: '3', label: 'Stakeholder Meeting' },
-                { id: '4', label: 'Scoping & Estimations' },
-                { id: '5', label: 'Sprint Showcase' },
-              ]}
-            />
-          </Grid>
-        </Grid>
-      </Container>
-    </>
+      </Card>
+    </CardContent>
+  </div>
   );
 }
